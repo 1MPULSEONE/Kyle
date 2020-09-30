245d7bb1d8836fcff4ded1da32b9a98cc54ae020
1MPULSEONE <parkhomenko2049@gmail.com>
2020-09-30 19:10:06 +0600

    BottomNavigation is connected with ViewPager.

--- app/src/main/java/com/lovejazz/kyle/MainActivity.java
+++ app/src/main/java/com/lovejazz/kyle/MainActivity.java
@@ -9,9 +9,14 @@ import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+    //Creating variable for bottomNavigation
+    private MenuItem prevMenuItem;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,10 +27,49 @@ public class MainActivity extends AppCompatActivity {
             Log.d("MainRegisterActivity","Toolbar produced null pointer exception");
         }
         setContentView(R.layout.activity_main);
-        //Connecting ViewPager with SectionPagerAdapter
-        SectionsPagerAdapter pagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
-        ViewPager pager = findViewById(R.id.main_pager);
+        //CreatingAdapter for ViewPager
+        final SectionsPagerAdapter pagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        final ViewPager pager = findViewById(R.id.main_pager);
         pager.setAdapter(pagerAdapter);
+        //BottomNavigation items react on clicks.
+        final BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.nav_home:
+                        pager.setCurrentItem(0);
+                        break;
+                    case R.id.nav_create:
+                        pager.setCurrentItem(1);
+                        break;
+                    case R.id.nav_settings:
+                        pager.setCurrentItem(2);
+                }
+                return false;
+            }
+        });
+        //Connecting ViewPager with BottomNavigationItem
+        pager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (prevMenuItem != null) {
+                    prevMenuItem.setChecked(false);
+                } else{
+                    bottomNavigationView.getMenu().getItem(0).setChecked(false);
+                }
+                bottomNavigationView.getMenu().getItem(position).setChecked(true);
+                prevMenuItem = bottomNavigationView.getMenu().getItem(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
     }
     //Creating adapter, which will provide information to ViewPager
     private static class SectionsPagerAdapter extends FragmentPagerAdapter {
