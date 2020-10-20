747229ef3351a9f4364a61414d7f105d139560fd
1MPULSEONE <parkhomenko2049@gmail.com>
2020-10-20 16:33:57 +0600

    Tabs added to fragment_create.xml. ArrowBack added to fragment_create.xml

--- app/src/main/java/com/lovejazz/kyle/SettingsFragment.java
+++ app/src/main/java/com/lovejazz/kyle/SettingsFragment.java
@@ -7,7 +7,6 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 public class SettingsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
