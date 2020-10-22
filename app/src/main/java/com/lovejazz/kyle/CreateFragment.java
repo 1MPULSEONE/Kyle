e30dd04be518fa440350b087a92e4b3d33a72fe1
1MPULSEONE <parkhomenko2049@gmail.com>
2020-10-23 00:04:03 +0600

    CreateFragment.java is updated. fragment_create.xml now have viewPager. strings.xml is updated. InformationFragment and SettingsOfCrateFragment are created.
    Signed-off-by: 1MPULSEONE <parkhomenko2049@gmail.com>

--- app/src/main/java/com/lovejazz/kyle/CreateFragment.java
+++ app/src/main/java/com/lovejazz/kyle/CreateFragment.java
@@ -2,17 +2,76 @@ package com.lovejazz.kyle;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.util.Objects;
 
 public class CreateFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create, container, false);
+        //Connecting pager with adapter
+        View RootView = inflater.inflate(R.layout.fragment_create, container, false);
+        final CreatePagerAdapter createPagerAdapter = new CreatePagerAdapter(Objects.requireNonNull(getFragmentManager()),0);
+        final ViewPager pager = RootView.findViewById(R.id.create_pager);
+        pager.setAdapter(createPagerAdapter);
+        //Creating infoButton listener
+        Button infoBtn = RootView.findViewById(R.id.button_info);
+        infoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pager.setCurrentItem(0);
+            }
+        });
+        //Creating settingsButton listener
+        Button settingsButton = RootView.findViewById(R.id.button_settings);
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pager.setCurrentItem(1);
+            }
+        });
+        return RootView;
+    }
+
+
+    private static class CreatePagerAdapter extends FragmentPagerAdapter {
+
+        public CreatePagerAdapter(@NonNull FragmentManager fm, int behavior) {
+            super(fm, behavior);
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            switch (position){
+                case 0:
+                    return new InformationFragment();
+                case 1:
+                    return new SettingsOfCreateFragment();
+            }
+            return null;
+        }
+
+        @Override
+        public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+            return false;
+        }
     }
 }
---
+++ app/src/main/java/com/lovejazz/kyle/SettingsOfCreateFragment.java
@@ -0,0 +1,19 @@
+package com.lovejazz.kyle;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SettingsOfCreateFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_settings_of_create, container, false);
+    }
+}
