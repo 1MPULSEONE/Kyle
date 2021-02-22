49f98eb068f4be300733218f327c5bf148a8c44e
“Parkhomenko <parkhomenko2049@gmail.com>
2021-02-22 10:36:12 +0600

    PasswordsFragment changes -Moved MainActivity`s logic to PasswordsFragment.java -Small changes in activity_main.xml and fragment_passwords.xml
    Signed-off-by: “Parkhomenko <parkhomenko2049@gmail.com>

--- app/src/main/java/com/lovejazz/kyle/MainActivity.java
+++ app/src/main/java/com/lovejazz/kyle/MainActivity.java
@@ -5,14 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -75,67 +72,12 @@ public class MainActivity extends AppCompatActivity {
             public void onPageScrollStateChanged(int state) {
             }
         });
-
-        //Connecting folder`s RecyclerView
-        RecyclerView folderRecycler = findViewById(R.id.folder_recycler);
-        String[] foldersName = new String[Folder.folders.length];
-        for (int i = 0; i < foldersName.length; i++) {
-            foldersName[i] = Folder.folders[i].getName();
-        }
-        int[] foldersImages = new int[Folder.folders.length];
-        for (int i = 0; i < foldersImages.length; i++) {
-            foldersImages[i] = Folder.folders[i].getImageRecourseId();
-        }
-        FolderAdapter folderAdapter = new FolderAdapter(foldersName, foldersImages);
-        folderRecycler.setAdapter(folderAdapter);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
-        folderRecycler.setLayoutManager(layoutManager);
-        //Connecting credit card`s RecyclerView
-        RecyclerView creditRecycler = findViewById(R.id.credit_card_recycler);
-        String[] creditsName = new String[CreditCard.creditCards.length];
-        for (int i = 0; i < creditsName.length; i++) {
-            creditsName[i] = CreditCard.creditCards[i].getName();
-        }
-        String[] creditsLogoNames = new String[CreditCard.creditCards.length];
-        for (int i = 0; i < creditsName.length; i++) {
-            creditsLogoNames[i] = CreditCard.creditCards[i].getLogoName();
-        }
-        int[] gradientId = new int[CreditCard.creditCards.length];
-        for (int i = 0; i < gradientId.length; i++) {
-            gradientId[i] = CreditCard.creditCards[i].getGradientId();
-        }
-        CreditCardAdapter creditCardAdapter = new CreditCardAdapter(creditsName, creditsLogoNames, gradientId);
-        creditRecycler.setAdapter(creditCardAdapter);
-        LinearLayoutManager cardManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
-        creditRecycler.setLayoutManager(cardManager);
-        //Connecting credit card`s RecyclerView
-        RecyclerView accountRecycler = findViewById(R.id.account_recycler);
-        String[] accountsName = new String[Account.accounts.length];
-        for (int i = 0; i < accountsName.length; i++) {
-            accountsName[i] = Account.accounts[i].getName();
-        }
-        String[] accountsLogins = new String[Account.accounts.length];
-        for (int i = 0; i < accountsLogins.length; i++) {
-            accountsLogins[i] = Account.accounts[i].getLogin();
-        }
-        int[] imageId = new int[Account.accounts.length];
-        for (int i = 0; i < imageId.length; i++) {
-            imageId[i] = Account.accounts[i].getImageId();
-        }
-        AccountAdapter accountAdapter = new AccountAdapter(accountsName, accountsLogins, imageId);
-        accountRecycler.setAdapter(accountAdapter);
-        LinearLayoutManager accountManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
-        accountRecycler.setLayoutManager(accountManager);
-    }
-
-    public void onSettingsButtonClicked(View view) {
-        //SettingButtonClicked
     }
 
     //Creating adapter, which will provide information to ViewPager
     private static class SectionsPagerAdapter extends FragmentPagerAdapter {
         public SectionsPagerAdapter(@NonNull FragmentManager fm) {
-            super(fm);
+            super(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
         }
 
         @NonNull
@@ -149,7 +91,7 @@ public class MainActivity extends AppCompatActivity {
                 case 2:
                     return new SettingsFragment();
             }
-            return null;
+            return new PasswordsFragment();
         }
 
         //Returns the number of fragments
--- app/src/main/java/com/lovejazz/kyle/PasswordsFragment.java
+++ app/src/main/java/com/lovejazz/kyle/PasswordsFragment.java
@@ -3,6 +3,8 @@ package com.lovejazz.kyle;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,7 +15,58 @@ public class PasswordsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_passwords, container, false);
-
+        View view = inflater.inflate(R.layout.fragment_passwords, container, false);
+        //Connecting folder`s RecyclerView
+        RecyclerView folderRecycler = view.findViewById(R.id.folder_recycler);
+        String[] foldersName = new String[Folder.folders.length];
+        for (int i = 0; i < foldersName.length; i++) {
+            foldersName[i] = Folder.folders[i].getName();
+        }
+        int[] foldersImages = new int[Folder.folders.length];
+        for (int i = 0; i < foldersImages.length; i++) {
+            foldersImages[i] = Folder.folders[i].getImageRecourseId();
+        }
+        FolderAdapter folderAdapter = new FolderAdapter(foldersName, foldersImages);
+        folderRecycler.setAdapter(folderAdapter);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(view.getContext(), LinearLayoutManager.HORIZONTAL, false);
+        folderRecycler.setLayoutManager(layoutManager);
+        //Connecting credit card`s RecyclerView
+        RecyclerView creditRecycler = view.findViewById(R.id.credit_card_recycler);
+        String[] creditsName = new String[CreditCard.creditCards.length];
+        for (int i = 0; i < creditsName.length; i++) {
+            creditsName[i] = CreditCard.creditCards[i].getName();
+        }
+        String[] creditsLogoNames = new String[CreditCard.creditCards.length];
+        for (int i = 0; i < creditsName.length; i++) {
+            creditsLogoNames[i] = CreditCard.creditCards[i].getLogoName();
+        }
+        int[] gradientId = new int[CreditCard.creditCards.length];
+        for (int i = 0; i < gradientId.length; i++) {
+            gradientId[i] = CreditCard.creditCards[i].getGradientId();
+        }
+        CreditCardAdapter creditCardAdapter = new CreditCardAdapter(creditsName, creditsLogoNames, gradientId);
+        creditRecycler.setAdapter(creditCardAdapter);
+        LinearLayoutManager cardManager = new LinearLayoutManager(view.getContext(), LinearLayoutManager.HORIZONTAL, false);
+        creditRecycler.setLayoutManager(cardManager);
+        //Connecting credit card`s RecyclerView
+        RecyclerView accountRecycler = view.findViewById(R.id.account_recycler);
+        String[] accountsName = new String[Account.accounts.length];
+        for (int i = 0; i < accountsName.length; i++) {
+            accountsName[i] = Account.accounts[i].getName();
+        }
+        String[] accountsLogins = new String[Account.accounts.length];
+        for (int i = 0; i < accountsLogins.length; i++) {
+            accountsLogins[i] = Account.accounts[i].getLogin();
+        }
+        int[] imageId = new int[Account.accounts.length];
+        for (int i = 0; i < imageId.length; i++) {
+            imageId[i] = Account.accounts[i].getImageId();
+        }
+        AccountAdapter accountAdapter = new AccountAdapter(accountsName, accountsLogins, imageId);
+        accountRecycler.setAdapter(accountAdapter);
+        LinearLayoutManager accountManager = new LinearLayoutManager(view.getContext(), LinearLayoutManager.VERTICAL, false);
+        accountRecycler.setLayoutManager(accountManager);
+        return view;
     }
+
 }
