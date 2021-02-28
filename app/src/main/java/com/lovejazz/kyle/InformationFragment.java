f5415928b7d18325c7b53316f9047e548f75cf5e
“Parkhomenko <parkhomenko2049@gmail.com>
2021-02-28 12:34:35 +0600

    FragmentInformation changes -Added spinner to fragment_information.xml -Added new strings for spinner in strings.xml -Changed AndroidManifest.xml -Created new layouts for spinners
    Signed-off-by: “Parkhomenko <parkhomenko2049@gmail.com>

--- app/src/main/java/com/lovejazz/kyle/InformationFragment.java
+++ app/src/main/java/com/lovejazz/kyle/InformationFragment.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class InformationFragment extends Fragment {
 
@@ -14,7 +16,13 @@ public class InformationFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_information, container, false);
-
+        View inflaterView = inflater.inflate(R.layout.fragment_information, container,
+                false);
+        Spinner typeSpinner = inflaterView.findViewById(R.id.type_spinner);
+        ArrayAdapter<CharSequence> typeSpinnerAdapter = ArrayAdapter.createFromResource(
+                inflaterView.getContext(), R.array.type_spinner,R.layout.spinner_item);
+        typeSpinnerAdapter.setDropDownViewResource(R.layout.spinner_item);
+        typeSpinner.setAdapter(typeSpinnerAdapter);
+        return inflaterView;
     }
 }
--- app/src/main/java/com/lovejazz/kyle/MainActivity.java
+++ app/src/main/java/com/lovejazz/kyle/MainActivity.java
@@ -2,7 +2,6 @@ package com.lovejazz.kyle;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
